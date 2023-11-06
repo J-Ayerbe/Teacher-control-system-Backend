25701d567f3b7dae9a0131ba87be26303728ef60
freider45 <freiderescobar@unicauca.edu.co>
2023-11-05 22:14:11 -0500

    Add notification

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import { notificationRouter } from './routes/notificationRoute';
 const app = express();
 app.use(express.json());
 app.use('/api', userRouter);
-
 app.use('/apiNotification', notificationRouter);
 
 app.listen(3000, () => {

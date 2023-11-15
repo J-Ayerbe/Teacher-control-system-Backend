5bb2a8d59761cb7bf778b3313a77165d9b28a8f1
freider45 <freiderescobar@unicauca.edu.co>
2023-11-15 09:00:23 -0500

    Add EducatorController

---
+++ src/types/IeducatorController.ts
@@ -0,0 +1,10 @@
+//Interface para edutacatorController
+import { Request,Response } from 'express';
+
+export interface IEducatorController {
+    getEducators(req: Request, res: Response): Promise<void>;
+    getEducatorById(req: Request, res: Response): Promise<void>;
+    createEducator(req: Request, res: Response): Promise<void>;
+    updateEducator(req: Request, res: Response): Promise<void>;
+    deleteEducator(req: Request, res: Response): Promise<void>;
+}

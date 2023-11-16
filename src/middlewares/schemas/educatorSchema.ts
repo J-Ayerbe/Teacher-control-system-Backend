e534abd78b1867ee17d849e48a9275c4f9fd74d4
J-Ayerbe <wesker0238@protonmail.com>
2023-11-15 23:09:37 -0500

    feat: Add AutoEvaluation import and update id validation in educatorSchema
    The commit adds an import statement for the AutoEvaluation interface in authController.ts. Additionally, it updates the id validation in educatorSchema.ts to expect a string with a minimum length of 5 characters and a maximum length of 11 characters.

--- src/middlewares/schemas/educatorSchema.ts
+++ src/middlewares/schemas/educatorSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const userSchema = z.object({
-  id: z.number().int().gte(1000).lte(9999999999),
+  id: z.string().min(5).max(11),
   email: z.string().email("Not a valid email"),
   password: z.string().min(5, {
     message: "Password must be at least 5 characters long",
